148dd343a134aaa4931ac37e42e693f7fe8cf864
WanderSmits <wandersmits@gmail.com>
2020-02-20 11:01:03 +0100

    Everything except node_moduels

---
+++ index.js
@@ -0,0 +1,7 @@
+const toTitleCase = require("to-title-case");
+const lower = "The lord of the rings: Return of the king";
+const title = toTitleCase(lower);
+
+console.log(toTitleCase);
+
+console.log(title);
